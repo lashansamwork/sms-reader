b01d05cac24ea19b97e516bc6b449e522b7846e8
Damindu <damindu7@gmail.com>
2022-09-25 20:13:08 +0530

    updated via code/ui

--- components/FormContent.js
+++ components/FormContent.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import { View, Text, TextInput } from "react-native";
 import layout from "../theme/layout";
 
-const FormContent = ({label, onChange, value }) => {
+const FormContent = ({label, value, passData}) => {
+    
+    const[textofInput, setTextofInput] = useState("")
+
+    // const changeHandler = text => {
+    //     setTextofInput(text)
+    //     passData(textofInput)
+    //     console.log(text)
+    // };
+ 
+
+    
+
     return (
-        <View
-            style={{
-                padding: layout.padding.medium,
-                flexDirection: 'column',
-                alignItems: 'center',
-                width: '100%',
-            }}>
+        <View style={{ padding: layout.padding.medium, flexDirection: 'column', alignItems: 'center', width: '100%', }}>
             <Text >{label}</Text>
-            <TextInput value={value} onChange={onChange} style={{ width: '60%', borderWidth: 1, borderRadius: layout.radius.regular }} />
+            <TextInput value={value} onChangeText={passData} style={{ width: '60%', borderWidth: 1, borderRadius: layout.radius.regular }} />
         </View>
     );
 }
